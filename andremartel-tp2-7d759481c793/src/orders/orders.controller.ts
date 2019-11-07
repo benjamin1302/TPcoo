3dffed7e1857f0142bbc83821eb9adc1b8438e19
benjaminDelbreuve <benjamindelbreuve@localhost.localdomain>
2019-11-07 15:13:17 +0000

    question 5

--- andremartel-tp2-7d759481c793/src/orders/orders.controller.ts
+++ andremartel-tp2-7d759481c793/src/orders/orders.controller.ts
@@ -37,28 +37,22 @@ export default class OrdersController {
 
   public getAll = async (request: Request, response: Response) => {
     let orders: Order[] = await this.orderService.getAll()
-    for( let order of orders)
-    {
-      order = this.orderBuilder.setAnomyne(order)
-    }
     response.json(orders).status(201)
   }
 
   public getById = async (request: Request, response: Response) => {
     const id = request.params.id
     // tslint:disable-next-line: triple-equals
-    let foundOrder: Order = await this.orderService.getById(Number(id))
+    const foundOrder: Order = await this.orderService.getById(Number(id))
     if (!foundOrder) {
       return response.sendStatus(404)
     }
-    foundOrder = this.orderBuilder.setAnomyne(foundOrder)
     response.json(foundOrder)
   }
 
   public create = async (request: Request, response: Response) => {
     let newOrder =  await this.orderService.create(request.body)
-    let orderToPrint = this.orderBuilder.setAnomyne(newOrder)
-    response.status(201).json(orderToPrint)
+    response.status(201).json(newOrder)
   }
 
   public delete = async (request: Request, response: Response) => {
